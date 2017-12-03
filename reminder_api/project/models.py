c49d674ee51e2e816e0492e21987fcacf8bc54c2
Rémi REY <rrey94@gmail.com>
2017-12-03 09:55:29 +0100

    add constraints on project name and environment name

--- reminder_api/project/models.py
+++ reminder_api/project/models.py
@@ -5,17 +5,20 @@ from django.db import models
 
 
 class Project(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return "name: %s" % self.name
 
 
 class Environment(models.Model):
+    name = models.CharField(max_length=20)
     project = models.ForeignKey(Project, related_name="environments")
     reminder = models.ForeignKey('reminder.Reminder')
     inventory = models.ForeignKey('inventory.Inventory')
-    name = models.CharField(max_length=20)
+
+    class Meta:
+        unique_together = ('project', 'name', )
 
     def __str__(self):
         return "name: %s" % self.name
